2f29724289b5d90ab35f639817217463c841177c
Kostas Chatzikokolakis <kostas@chatzi.org>
2016-03-14 15:31:09 +0100

    loss.h: allow passing loss function as Metric

--- include/qif_bits/loss.h
+++ include/qif_bits/loss.h
@@ -31,6 +31,55 @@ arma::ucolvec strategy(const Mat<eT>& L, const Prob<eT>& pi, const Chan<eT>& C) 
 	return g::strategy<eT>(-L, pi, C);
 }
 
+
+// same methods, with Metric loss instead of Mat L.
+// Guesses are assumed to be the inputs
+//
+template<typename eT>
+inline
+Mat<eT> metric_to_mat(Metric<eT, uint> l, uint n) {
+	Mat<eT> L(n, n);
+	for(uint i = 0; i < n; i++)
+		for(uint j = 0; j < n; j++)
+			L(i, j) = l(i, j);
+	return L;
+}
+
+template<typename eT>
+eT entropy(const Metric<eT, uint>& l, const Prob<eT>& pi) {
+	return entropy(metric_to_mat(l, pi.n_cols), pi);
+}
+
+template<typename eT>
+eT post_entropy(const Metric<eT, uint>& l, const Prob<eT>& pi, const Chan<eT>& C) {
+	return post_entropy(metric_to_mat(l, pi.n_cols), pi, C);
+}
+
+template<typename eT>
+eT add_leakage(const Metric<eT, uint>& l, const Prob<eT>& pi, const Chan<eT>& C) {
+	return add_leakage(metric_to_mat(l, pi.n_cols), pi, C);
+}
+
+template<typename eT>
+eT mult_leakage(const Metric<eT, uint>& l, const Prob<eT>& pi, const Chan<eT>& C) {
+	return mult_leakage(metric_to_mat(l, pi.n_cols), pi, C);
+}
+
+template<typename eT>
+eT mulg_leakage(const Metric<eT, uint>& l, const Prob<eT>& pi, const Chan<eT>& C) {
+	return mulg_leakage(metric_to_mat(l, pi.n_cols), pi, C);
+}
+
+template<typename eT>
+arma::ucolvec strategy(const Metric<eT, uint>& l, const Prob<eT>& pi, const Chan<eT>& C) {
+	return strategy(metric_to_mat(l, pi.n_cols), pi, C);
+}
+
+
+
+
+
+
 /*
 OLD CODE, we now use the g:: methods with -L
 
