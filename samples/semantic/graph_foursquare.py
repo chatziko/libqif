80f21c06f1a6dcdbaef1d920556354599d671473
susheels <susheel.suresh@gmail.com>
2016-07-30 21:14:15 +0200

    functionality added for both datasets

--- samples/semantic/graph_foursquare.py
+++ samples/semantic/graph_foursquare.py
@@ -18,25 +18,15 @@ def graphlaplace():
 			c = c+1
 	pyplot.show()
 
-def graphprivacy(): # adverror
-	dataset1 = "generated_data_foursquare/laplace-1.732868-tky.csv-foursquare-euclidean"
-	dataset2 = "generated_data_foursquare/laplace-Government Building-1.732868-tky.csv-foursquare-euclidean"
-	dataset3 = "generated_data_foursquare/laplace-Airport-1.732868-tky.csv-foursquare-euclidean"
-	dataset4 = "generated_data_foursquare/laplace-Movie Theater-1.732868-tky.csv-foursquare-euclidean"
-	dataset6 = "generated_data_foursquare/laplace-Restaurant-1.732868-tky.csv-foursquare-euclidean"
-	dataset7 = "generated_data_foursquare/laplace-Indian Restaurant-1.732868-tky.csv-foursquare-euclidean"
-	dataset8 = "generated_data_foursquare/laplace-Dumpling Restaurant-1.732868-tky.csv-foursquare-euclidean"
-	dataset9 = "generated_data_foursquare/laplace-French Restaurant-1.732868-tky.csv-foursquare-euclidean"
-	dataset10 = "generated_data_foursquare/laplace-Dessert Shop-1.732868-tky.csv-foursquare-euclidean"
-
-	dataset = [dataset1,dataset2,dataset3,dataset4,dataset6,dataset7,dataset8,dataset9,dataset10]
-
+def graphprivacy(dataset_name, gen_data): # adverror
+	
+	
 	data_to_plot = []
-
-	for i in dataset:
+	xticklabels = []
+	for i in gen_data:
 		content = []
-		with open(i) as f:
-			content = f.readlines()
+		with open("generated_data_foursquare/"+i) as f:
+			content = f.readlines()			
 			print len(content)
 		adverror = []
 		for z in content:
@@ -44,18 +34,54 @@ def graphprivacy(): # adverror
 		y = np.asarray(adverror)
 		data_to_plot.append(y)
 
+		l = i.split('-')
+		if len(l) == 5:
+			xticklabels.append('PL (Without labels)')
+		else:
+			xticklabels.append(l[1])
 
-	fig = pyplot.figure(1,figsize=(14, 8))
+
+
+	fig = pyplot.figure(1,figsize=(21, 8))
+	fig.suptitle(dataset_name+' Dataset', fontsize=14, fontweight='bold')
 	ax = fig.add_subplot(111)
 
-	bp = ax.boxplot(data_to_plot,widths=0.4,showmeans=True)
-	ax.set_xticklabels(['PL', 'Govt. Building','Airport','Movie Theater','Restaurant','Indian R','Dumpling R','French R','Dessert Shop'])
-	
+	bp = ax.boxplot(data_to_plot,widths=0.3,showmeans=True)
+	ax.set_xticklabels(xticklabels)
+	ax.set_xlabel('Semantic Labels')
+	ax.set_ylabel('AdvError')
  	
  	pyplot.axhline(y=bp['boxes'][0].get_ydata()[2],linewidth=0.5)
-	fig.savefig('tky_foursquare.png', bbox_inches='tight')
+	fig.savefig(dataset_name+'-foursquare.png', bbox_inches='tight')
 	pyplot.show()
 
 
-graphprivacy()
+
+#################### Config ###########################
+
+
+gen_data_tky = ['laplace-1.732868-tky.csv-foursquare-euclidean',
+'laplace-Government Building-1.732868-tky.csv-foursquare-euclidean', 
+'laplace-Airport-1.732868-tky.csv-foursquare-euclidean',
+'laplace-Movie Theater-1.732868-tky.csv-foursquare-euclidean',
+'laplace-Restaurant-1.732868-tky.csv-foursquare-euclidean',
+'laplace-Indian Restaurant-1.732868-tky.csv-foursquare-euclidean',
+'laplace-Dumpling Restaurant-1.732868-tky.csv-foursquare-euclidean',
+'laplace-French Restaurant-1.732868-tky.csv-foursquare-euclidean',
+'laplace-Dessert Shop-1.732868-tky.csv-foursquare-euclidean']
+
+gen_data_nyc = ['laplace-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-Airport-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-Stadium-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-Train Station-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-American Restaurant-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-Italian Restaurant-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-French Restaurant-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-Chinese Restaurant-1.386294-nyc.csv-foursquare-euclidean',
+'laplace-Government Building-1.386294-nyc.csv-foursquare-euclidean']
+
+
+#graphprivacy('Tokyo',gen_data_tky)
+graphprivacy('New York',gen_data_nyc)
+
 #graphlaplace()
