20fd781cb5c5f209f2010d3e4cffda241ddd82f2
kassemal <alikassem20@gmail.com>
2017-10-12 15:50:31 +0200

    Update plots.py
    minor changes

--- samples/optimal-ValueTools17/plots.py
+++ samples/optimal-ValueTools17/plots.py
@@ -4,7 +4,6 @@ subplot1: a graph with several curves for Time w.r.t number of locations
 subplot2: a graph with several curves for Utility Loss w.r.t number of locations
 Each curve corresponds to the Optimal method or "Kostas" method with certain Radius (R) 
 """
-
 # !/usr/bin/env python
 # coding=utf-8
 
@@ -36,7 +35,7 @@ for i, line in enumerate(data_file):
         continue
     else:
         if counter == 0:
-            locations.append(int(temp[0]))
+            locations.append(temp[0])
         try:
             Utility_L[labels[counter]].append(float(temp[2]))
         except KeyError:
@@ -46,8 +45,7 @@ for i, line in enumerate(data_file):
         except KeyError:
             c_Time[labels[counter]] = [float(temp[3])]
 
-
-xValues = np.arange(1, len(locations)+1, 1)
+#xValues = np.array(range(1, len(locations)+1))
 CT= [None for x in labels]
 UL = [None for x in labels]
 fig, axs = plt.subplots(1, 2, figsize=(8.4, 4.3))
@@ -55,8 +53,8 @@ fig, axs = plt.subplots(1, 2, figsize=(8.4, 4.3))
 #Subplot 2: Time wrt nb. of loc.
 for i in range(len(labels)):
     c_t = np.array(c_Time[labels[i]])
-    CT[i], = axs[0].plot(xValues, c_t, color=l_colors[i], ls='-', marker=l_mark[i], markeredgecolor=l_colors[i])
-axs[0].set_xticklabels(locations, rotation=45)
+    CT[i], = axs[0].plot(locations, c_t, color=l_colors[i], ls='-', marker=l_mark[i], markeredgecolor=l_colors[i])
+#axs[0].set_xticklabels(locations, rotation=45)
 axs[0].set_ylabel('Time (minutes)', fontsize=16)
 axs[0].set_xlabel('Number of Locations', fontsize=16)
 leg = axs[0].legend(CT, labels, loc=0, fontsize=10.5)
@@ -65,10 +63,10 @@ leg = axs[0].legend(CT, labels, loc=0, fontsize=10.5)
 #Subplot 2: Utility Loss wrt nb. of loc.
 for i in range(len(labels)):
     ul = np.array(Utility_L[labels[i]])
-    UL[i], = axs[1].plot(xValues, ul, color=l_colors[i], ls='-', marker=l_mark[i], markeredgecolor=l_colors[i])
+    UL[i], = axs[1].plot(locations, ul, color=l_colors[i], ls='-', marker=l_mark[i], markeredgecolor=l_colors[i])
 axs[1].set_ylabel('Utility Loss', fontsize=16)
 axs[1].yaxis.set_label_position("right")
-axs[1].set_xticklabels(locations, rotation=45)
+#axs[1].set_xticklabels(locations, rotation=45)
 axs[1].set_xlabel('Number of Locations', fontsize=16)
 leg = axs[1].legend(UL, labels, loc=0, fontsize=10.5)
 
