d5dbea4a8230892c6fca3f9b2e958f063f37ad79
Kostas Chatzikokolakis <kostas@chatzi.org>
2018-04-10 02:18:36 +0200

    inline rng functions

--- include/qif_bits/rng.h
+++ include/qif_bits/rng.h
@@ -1,12 +1,14 @@
 
 namespace rng {
 
+inline
 void set_seed(uint seed) {
 
 	arma::arma_rng::set_seed(seed);
 	std::srand(seed);
 }
 
+inline
 void set_seed_random() {
 	arma::arma_rng::set_seed_random();
 
