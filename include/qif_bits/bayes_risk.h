408f922fd8dd6bdc4f112ed41fe20c378126b324
Kostas Chatzikokolakis <kostas@chatzi.org>
2019-07-28 20:03:38 +0300

    bayes_risk: capacity via tv, linf version, bounds

--- include/qif_bits/bayes_risk.h
+++ include/qif_bits/bayes_risk.h
@@ -31,25 +31,105 @@ eT mult_leakage(const Prob<eT>& pi, const Chan<eT>& C) {
 // (still not 100% sure whether this is true)
 //
 template<typename eT>
-eT mult_capacity(const Chan<eT>& C) {
-	Chan<eT> Csmall(2, C.n_cols);
+eT mult_capacity(const Chan<eT>& C, uint& res_x1, uint& res_x2) {
+	auto tv = metric::total_variation<eT, Prob<eT>>();
+	eT tv_max(0);
 
-	eT max(0);
 	for(uint x1 = 0; x1 < C.n_rows; x1++) {
-		Csmall.row(0) = C.row(x1);
-
 		for(uint x2 = x1+1; x2 < C.n_rows; x2++) {
-			Csmall.row(1) = C.row(x2);
+			eT tv_cur = tv(C.row(x1), C.row(x2));
 
-			eT sum_of_maxima = arma::accu(arma::max(Csmall, 0));
-			if(equal(sum_of_maxima, eT(2))) return infinity<eT>();
+			if(less_than(tv_max, tv_cur)) {
+				tv_max = tv_cur;
+				res_x1 = x1;
+				res_x2 = x2;
 
-			eT l = eT(1) / (eT(2) - sum_of_maxima);
-			if(less_than(max, l))
-				max = l;
+				if(equal(tv_max, eT(1)))
+					return infinity<eT>();
+			}
 		}
 	}
-	return max;
+	return eT(1) / (eT(1) - tv_max);
+}
+
+template<typename eT>
+eT mult_capacity(const Chan<eT>& C) {
+	uint x1, x2;
+	return mult_capacity<eT>(C, x1, x2);
+}
+
+// Computes by mult_capacity by embeding C's rows in Rinf^d where d = 2^m (m is n_cols).
+// The embedding phi(x) in R^d of a vector x in R^m has one coordinate for every bitstring b in {0,1}^m.
+// The value of that coordinate is cdot(x, coeff) where coeff_i = (-1)^{b_i}.
+//
+// In Linf, the diamater is simply given by max_i (max_x x_i - min_x x_i). So for each coordinate b, we need to
+// compute { phi(x)_b }_x, then compute the max - min, and finally keep the maximum of those.
+//
+template<typename eT>
+eT mult_capacity_via_linf(const Chan<eT>& C) {
+	eT diam(0), one(1);
+
+	arma::Col<eT> coeff(C.n_cols);	// we only keep the coeff vector (b is implicit)
+	coeff.fill(one);				// start with b = 00..0, coeff = (1,...,1)
+
+	// we have a loop for every value of b. The loop finishes when b becomes 11..1, in which case
+	// the update loop (which stops at the first 0) will finish all steps and i will become C.n_cols
+	uint i = 0;
+	while(i < C.n_cols) {
+		// with a single multiplication we get cdot(coeff, row) for all rows.
+		// we need to compute max-min, and update diam if the new value is bigger
+		auto trans = C * coeff;
+		diam = max(diam, arma::max(trans) - arma::min(trans));
+
+		// compute the coeff for the next value of b. For b we would need to change 0s to 1s and vice versa, until we find the first 0 (which became 1).
+		// In coeff, this translates changing 1s to -1s and vice versa, until we find the first -1 (which became 1).
+		for(i = 0; i < C.n_cols && (coeff(i) *= -one) > eT(0); i++)
+			;
+	}
+
+	return one / (one - diam/eT(2));
+}
+
+template<typename eT>
+eT mult_capacity_bound1(const Chan<eT>& C) {
+	auto tv = metric::total_variation<eT, Prob<eT>>();
+	eT tv_max(0);
+
+	uint x1 = C.n_rows/2;		// TODO: rand?
+	for(uint x2 = x1+1; x2 < C.n_rows; x2++) {
+		eT tv_cur = tv(C.row(x1), C.row(x2));
+
+		if(less_than(tv_max, tv_cur)) {
+			tv_max = tv_cur;
+
+			if(equal(tv_max, eT(1)))
+				return infinity<eT>();
+		}
+	}
+
+	eT one(1);
+	eT bound(2 * tv_max);
+	return less_than(bound, one) ? one / (one - bound) : infinity<eT>();
+}
+
+template<typename eT>
+eT mult_capacity_bound2(const Chan<eT>& C) {
+	auto euclid = metric::euclidean<eT, Prob<eT>>();
+	eT euclid_max(0);
+
+	for(uint x1 = 0; x1 < C.n_rows; x1++) {
+		for(uint x2 = x1+1; x2 < C.n_rows; x2++) {
+			eT euclid_cur = euclid(C.row(x1), C.row(x2));
+
+			if(less_than(euclid_max, euclid_cur)) {
+				euclid_max = euclid_cur;
+			}
+		}
+	}
+
+	eT one(1);
+	eT bound(euclid_max * std::sqrt(C.n_cols) / 2);
+	return less_than(bound, one) ? one / (one - bound) : infinity<eT>();
 }
 
 template<typename eT>
