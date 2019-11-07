86ab247ab8693e8843b4a8fe50b1894433f39442
Kostas Chatzikokolakis <kostas@chatzi.org>
2019-11-07 18:33:54 +0100

    geo-opt: get prior as input

--- samples/geo-optimal-given-min-bayes-risk/geo-optimal-given-min-bayes-risk.cpp
+++ samples/geo-optimal-given-min-bayes-risk/geo-optimal-given-min-bayes-risk.cpp
@@ -11,8 +11,8 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	qif::rng::set_seed_random();		// RNG initialization
 
-	if(argc != 6) {
-		cout << "args should be: <width> <height> <cell_size> <min_bayes_risk> <max_loss>\n";
+	if(argc != 7) {
+		cout << "args should be: <width> <height> <cell_size> <min_bayes_risk> <max_loss> [<prior_file>|uniform]\n";
 		return -1;
 	}
 
@@ -20,12 +20,29 @@ int main(int argc, char* argv[]) {
 	uint height = std::stoi(argv[2]);
 	uint n = width * height;
 	double cell_size = std::stod(argv[3]);
-	prob pi = probab::uniform(n);
 
 	auto euclid = metric::euclidean<double, point>();
 	auto loss = cell_size * metric::compose(euclid, geo::cell_to_point(width));
 	double max_vuln = 1 - std::stod(argv[4]);
 	double max_loss = std::stod(argv[5]);
+	string pi_file = argv[6];
+
+	prob pi;
+	if(pi_file == "uniform")
+		pi = probab::uniform(n);
+	else {
+		pi.load(pi_file);
+		if(pi.is_empty()) {
+			cerr << "cannot open " << pi_file << "\n";
+			return 1;
+		}
+		if(pi.n_elem != n) {
+			cerr << "prior should have size " << n << "\n";
+			return 1;
+		}
+		if(!probab::is_proper(pi))
+			cerr << "warning: prior is not a proper distribution\n";
+	}
 
 	lp::Defaults::msg_level = lp::MsgLevel::ALL;
 
