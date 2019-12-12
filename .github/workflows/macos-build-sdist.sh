f8878995eb0b4b6ea58dff976878888ec11f44c0
Kostas Chatzikokolakis <kostas@chatzi.org>
2019-12-13 01:04:15 +0200

    gh-actions: add macos pypi build

---
+++ .github/workflows/macos-build-sdist.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export HOMEBREW_NO_INSTALL_CLEANUP=1	# make homebrew
+export HOMEBREW_NO_AUTO_UPDATE=1		# faster
+
+cmake --version
+
+rm -f /usr/local/include/c++	# brew install will fail if this exists
+
+brew install gsl gmp armadillo
+
+# or-tools
+(brew unlink protobuf; true)	# unlink protobuf if present so that it doesn't conflict with the one installed by or-tools
+brew tap chatziko/tap
+brew install --HEAD or-tools
+
+# build qif
+mkdir build
+cd build
+cmake ..
+make install -j 2
+
+# build source dist
+cd ../python
+
+python setup.py sdist
