7d5a6065a323f35b5ca0c43a8e54d2a2d617801f
Kostas Chatzikokolakis <kostas@chatzi.org>
2019-12-12 09:43:08 +0200

    python: first pip installable version

---
+++ python/qif/__init__.py
@@ -0,0 +1,1 @@
+from qif import channel, probab, metric
--- python/setup.py
+++ python/setup.py
@@ -1,119 +1,78 @@
+import os
+import re
+import sys
+import platform
+import subprocess
+
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import sys
-import setuptools
+from distutils.version import LooseVersion
 
 
-#### TODO: make this work with cmake
-
-__version__ = '0.0.1'
-
-
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
+class CMakeExtension(Extension):
+	def __init__(self, name, sourcedir=''):
+		Extension.__init__(self, name, sources=[])
+		self.sourcedir = os.path.abspath(sourcedir)
 
 
-ext_modules = [
-    Extension(
-        'python_example',
-        ['src/main.cpp'],
-        depends=['src/armadillo.h'],
-        include_dirs=[
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True)
-        ],
-        libraries=["armadillo", "qif", "mp++", "gmp"],
-        language='c++'
-    ),
-]
+class CMakeBuild(build_ext):
+	def run(self):
+		try:
+			out = subprocess.check_output(['cmake', '--version'])
+		except OSError:
+			raise RuntimeError("CMake must be installed to build the following extensions: " +
+							   ", ".join(e.name for e in self.extensions))
+
+		if platform.system() == "Windows":
+			cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+			if cmake_version < '3.1.0':
+				raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+		for ext in self.extensions:
+			self.build_extension(ext)
+
+	def build_extension(self, ext):
+		extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+		cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+					  '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+		cfg = 'Debug' if self.debug else 'Release'
+		build_args = ['--config', cfg]
+
+		if platform.system() == "Windows":
+			cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+			if sys.maxsize > 2**32:
+				cmake_args += ['-A', 'x64']
+			build_args += ['--', '/m']
+		else:
+			cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+			build_args += ['--', '-j2']
+
+		env = os.environ.copy()
+		env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+															  self.distribution.get_version())
+		if not os.path.exists(self.build_temp):
+			os.makedirs(self.build_temp)
+		subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+		subprocess.check_call(['cmake', '--build', '.', '-t', os.path.basename(ext.name)] + build_args, cwd=self.build_temp)
 
 
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14/17] compiler flag.
-
-    The newer version is prefered over c++11 (when it is available).
-    """
-    flags = ['-std=c++17']
-
-    for flag in flags:
-        if has_flag(compiler, flag): return flag
-
-    raise RuntimeError('Unsupported compiler -- at least C++17 support '
-                       'is needed!')
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-    l_opts = {
-        'msvc': [],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        c_opts['unix'] += darwin_opts
-        l_opts['unix'] += darwin_opts
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        link_opts = self.l_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-            ext.extra_link_args = link_opts
-        build_ext.build_extensions(self)
 
 setup(
-    name='python_example',
-    version=__version__,
-    author='Sylvain Corlay',
-    author_email='sylvain.corlay@gmail.com',
-    url='https://github.com/pybind/python_example',
-    description='A test project using pybind11',
-    long_description='',
-    ext_modules=ext_modules,
-    install_requires=['pybind11>=2.4'],
-    setup_requires=['pybind11>=2.4'],
-    cmdclass={'build_ext': BuildExt},
-    zip_safe=False,
-    test_suite='tests/test.py',
+	name='qif',
+	version='0.1.1',
+	author='Kostas Chatzikokolakis  ',
+	author_email='kostas@chatzi.org',
+	url='https://github.com/chatziko/libqif',
+	description='Quantitative Information Flow library',
+	long_description='Quantitative Information Flow library',
+	packages=['qif'],
+	ext_modules=[
+		CMakeExtension('qif/channel'),
+		CMakeExtension('qif/probab'),
+		CMakeExtension('qif/metric'),
+	],
+	cmdclass=dict(build_ext=CMakeBuild),
+	zip_safe=False,
+	test_suite='tests/test.py',
 )
