cd36dc40db38d2b5fc94a0fd2903dfac6fc26562
Kostas Chatzikokolakis <kostas@chatzi.org>
2020-12-13 13:44:42 +0200

    gh-actions: use cibuildwheel

---
+++ .github/workflows/ubuntu-build-qif.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# stop on any error
+set -e
+
+cat /proc/cpuinfo
+
+# Sphinx needed for docs, numpy to import the qif module while generating docs
+# TODO: remove this after generating new docker image
+/opt/python/cp36-cp36m/bin/pip install Sphinx numpy
+
+# build qif
+mkdir build
+cd build
+cmake -DMARCH=x86-64 -DPYTHON_EXECUTABLE=/opt/python/cp36-cp36m/bin/python ..		# the gh-actions vm crashes with march=native,sandybridge or haswell, so use x86-64 just for the tests
+make qif tests samples docs -j 2
+./tests/run
+
+mv misc/docs/_build/html ..															# save in case we need to publish it
+touch ../html/.nojekyll																# disable jekyll processing, cause it hides folders starting with underscore!
+
+rm -rf *
+cmake -DPYTHON_EXECUTABLE=/opt/python/cp36-cp36m/bin/python ..						# then compile again with the default march
+make install -j 2
