43e3948abc808f009392e67f4b112d3c478a76df
Kostas Chatzikokolakis <kostas@chatzi.org>
2020-12-20 16:09:41 +0200

    upgrade armadillo to 10.1.2, with fixes to support it

--- lib/include/qif_bits/arma_rat.h
+++ lib/include/qif_bits/arma_rat.h
@@ -90,16 +90,36 @@ arma_rng_cxx98::randn_dual_val(rat&, rat&) {
 	throw "not implemented";
 }
 
-} // namespace arma
-
-
-namespace arma::priv {
-
-// Max positive number, set to a "sufficiently high" value
+// Datum<eT> declares various constexpr constants, but a constexpr cannot have type rat.
+// So we specialize the class and declare the same constants as inline const.
 //
 template<>
-arma_inline rat most_pos<rat>(rat*) {
-	return qif::infinity<rat>();
-}
+class Datum<rat>
+  {
+  public:
+  static const rat pi;       //!< ratio of any circle's circumference to its diamrater
+  static const rat e;        //!< base of the natural logarithm
+  static const rat euler;    //!< Euler's constant, aka Euler-Mascheroni constant
+  static const rat gratio;   //!< golden ratio
+  static const rat sqrt2;    //!< square root of 2
+  static const rat sqrt2pi;  //!< square root of 2*pi
+  static const rat eps;      //!< the difference bratween 1 and the least value greater than 1 that is representable
+  static const rat log_min;  //!< log of the minimum representable value
+  static const rat log_max;  //!< log of the maximum representable value
+  static const rat nan;      //!< "not a number"
+  static const rat inf;      //!< infinity 
+  };
+  
+inline const rat Datum<rat>::pi        = rat(Datum<double>::pi);
+inline const rat Datum<rat>::e         = rat(Datum<double>::e);
+inline const rat Datum<rat>::euler     = rat(Datum<double>::euler);
+inline const rat Datum<rat>::gratio    = rat(Datum<double>::gratio);
+inline const rat Datum<rat>::sqrt2     = rat(Datum<double>::sqrt2);
+inline const rat Datum<rat>::sqrt2pi   = rat(Datum<double>::sqrt2pi);
+inline const rat Datum<rat>::eps       = std::numeric_limits<rat>::epsilon();
+inline const rat Datum<rat>::log_min   = rat(Datum<double>::log_min);
+inline const rat Datum<rat>::log_max   = rat(Datum<double>::log_max);
+inline const rat Datum<rat>::nan       = priv::Datum_helper::nan<rat>();
+inline const rat Datum<rat>::inf       = priv::Datum_helper::inf<rat>();
 
-} // namespace arma::priv
+} // namespace arma
