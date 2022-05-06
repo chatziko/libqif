5effb4c9c9d9417af8e39bac6fa8a00ef9d828f7
Kostas Chatzikokolakis <kostas@chatzi.org>
2022-05-06 11:55:55 +0300

    libqif-manylinux: upgrade to manylinux2014

--- misc/docker/libqif-manylinux/setup.sh
+++ misc/docker/libqif-manylinux/setup.sh
@@ -3,22 +3,13 @@
 # stop on any error
 set -e
 
-yum install -y wget dsl-devel gsl-devel xz
+yum install -y wget gsl-devel xz
 
-
-# cmake
-yum remove -y cmake
-
-cd /tmp
-wget -nv https://github.com/Kitware/CMake/releases/download/v3.16.1/cmake-3.16.1-Linux-x86_64.tar.gz
-
-tar -xf cmake-3.16.1-Linux-x86_64.tar.gz
-cp -r cmake*/* /usr/local
 
 # openblas
-wget -nv https://github.com/xianyi/OpenBLAS/archive/v0.3.7.zip
-unzip v0.3.7
-cd OpenBLAS-0.3.7
+wget -nv https://github.com/xianyi/OpenBLAS/archive/v0.3.20.zip
+unzip v0.3.20
+cd OpenBLAS-0.3.20
 make TARGET=SANDYBRIDGE
 make install PREFIX=/usr/local
 ln -s /usr/local/lib/libopenblas.so /usr/local/lib/liblapack.so		# openblas actually contains a lapack
@@ -26,9 +17,9 @@ ln -s /usr/local/lib/libopenblas.a /usr/local/lib/liblapack.a		# implementation,
 
 # gmp
 cd /tmp
-wget -nv https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz
+wget -nv https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
 tar -xf gmp*.tar.xz
-cd gmp-6.1.2
+cd gmp-6.2.1
 ./configure --build=sandybridge-pc-linux-gnu
 make install
 
@@ -41,12 +32,12 @@ cp -r or-tools*/include /usr/local
 cp or-tools*/lib/*.a /usr/local/lib
 
 # ...and we extract the shared libraries from the python lib!
-/opt/python/cp36-cp36m/bin/pip install ortools==8.1.8487
-cp /opt/_internal/cpython-3.6.12/lib/python3.6/site-packages/ortools/.libs/* /usr/local/lib
+/opt/python/cp37-cp37m/bin/pip install ortools==8.1.8487
+cp /opt/_internal/cpython-3.7.13/lib/python3.7/site-packages/ortools/.libs/* /usr/local/lib
 ldconfig
 
 # Sphinx needed for docs, numpy to import the qif module while generating docs
-/opt/python/cp36-cp36m/bin/pip install Sphinx numpy
+/opt/python/cp37-cp37m/bin/pip install Sphinx numpy
 
 # cleanup
 rm -rf /tmp/*
